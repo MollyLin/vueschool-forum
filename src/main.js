0c09e0c286f4a6860ba9d491b956398e3d023723
Molly <molly.lin@senao.com.tw>
2022-02-04 15:10:34 +0800

    Install and Configure Vue Router 4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/components/PageHome')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
